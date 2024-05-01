8af07c0174284cd19459cd38446f12ada422424b
MarkVanMortem <34596876+magister1101@users.noreply.github.com>
2024-05-02 01:22:16 +0800

    tileset

--- entity/character/player.gd
+++ entity/character/player.gd
@@ -1,15 +1,38 @@
 extends CharacterBody2D
 
 
-@export var speed: int = 50
+@export var speed: int = 50;
+@export var runningSpeed: int = 100;
+@export var animationPlayer: AnimationPlayer;
 
 
 
 func _physics_process(delta):
-	handleInput()
+	handleInput();
 	
 func handleInput():
 	var moveDirection = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down");
 	var isRunning = Input.is_action_pressed("running");
-	velocity = moveDirection*speed
-	move_and_slide()
+
+	animations();
+	
+	velocity = moveDirection*speed;
+	if isRunning:
+		velocity = moveDirection*runningSpeed;
+	move_and_slide();
+	
+func animations():
+	var play = "idle";
+	
+	if velocity.x > 0:
+		play = "walkRight";
+	elif velocity.x < 0:
+		play = "walkLeft";
+	elif velocity.y > 0:
+		play = "walkDown";
+	elif velocity.y < 0:
+		play = "walkUp";
+	
+	animationPlayer.play(play)
+	
+	
